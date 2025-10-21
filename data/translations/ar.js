d4847a4eff301c19aad9aa0786bfb3ea7303ccd7
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2025-10-21 21:30:43 +0500

    Update ar.js

--- data/translations/ar.js
+++ data/translations/ar.js
@@ -9,7 +9,7 @@ export default {
 		hireMe: 'وظفني'
 	},
 	banner: {
-		greeting: 'مرحباً، أنا شهارزاد',
+		greeting: 'السيد شهرزاد',
 		description: 'مطور واجهات أمامية | React.js | Node.js | متحمس لـ Python | LLM | بناء تطبيقات ويب قابلة للتوسع',
 		downloadCV: 'تحميل السيرة الذاتية',
 		bookCall: 'احجز مكالمة'
